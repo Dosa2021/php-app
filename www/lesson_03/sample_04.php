cb9281d79cb13b8f26ff6b6bedb210b8c10fc0b1
mel_sugawara <mel_sugawara@mel-mac-kinoshita.local>
2026-05-27 15:11:38 +0900

    prepareを使用してsql実行

--- www/lesson_03/sample_04.php
+++ www/lesson_03/sample_04.php
@@ -8,7 +8,13 @@
 <?php
     $db = new mysqli('mysql:3306', 'root', 'root_password', 'mydb');
     $message = 'フォームから入力した値';
-    $ret = $db->query('INSERT INTO memos (memo) VALUES ("phpからのメモです");');
+    $stmt = $db->prepare('INSERT INTO memos (memo) VALUES (?);');
+    if (!$stmt):
+        die($db->error);
+    endif;
+    $stmt->bind_param('s', $message);
+    // $ret = $db->query('INSERT INTO memos (memo) VALUES ("phpからのメモです");');
+    $ret = $stmt->execute();
     if ($ret) :
         echo 'insert 成功';
     else: 
