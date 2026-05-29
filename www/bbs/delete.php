480f18986b956ab8d7e2747927dab68337edaf1f
mel_sugawara <mel_sugawara@mel-mac-kinoshita.local>
2026-05-29 18:32:22 +0900

    削除機能作成

--- www/bbs/delete.php
+++ www/bbs/delete.php
@@ -1,3 +1,31 @@
 <?php
-header('Location: index.php'); exit();
+    require('lib/lib.php');
+    session_start();
+
+    $id = 0;
+    $name = '';
+
+    if (isset($_SESSION['id']) && isset($_SESSION['name'])) {
+        $id = $_SESSION['id'];
+        $name = $_SESSION['name'];
+    } else {
+        header('Location: login.php');
+        exit();
+    }
+
+    $post_id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+    if (!$post_id) {
+        header('Location: index.php');
+        exit();
+    }
+
+    $db = dbConnect();
+    $stmt = $db->prepare('DELETE FROM posts WHERE id = ? limit 1;');
+    $stmt->bind_param('i', $post_id);
+    $result = $stmt->execute();
+    if (!$result) {
+        die($db->error);
+    }
+
+    header('Location: index.php'); exit();
 ?>
